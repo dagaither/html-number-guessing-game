eb016df52953041ed67b61d0cf9db4a6116063ad
dagaither <gaither@protonmail.com>
2024-02-27 10:04:54 -0500

    Initial commit

--- app.js
+++ app.js
@@ -1,2 +1,45 @@
 console.log("HELLO PAPA PLATOON!")
 // Your function(s) should go here that will interact with the webpage or DOM
+
+// 
+let randNum = Math.floor(Math.random() * 100) + 1;
+
+
+document.getElementById("userInput").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    let userInput = parseInt(document.getElementById("userGuess").value);
+    if (userInput >=1 && userInput <=100) {
+        if (userInput > randNum) {
+            appendGuess(userInput, "high")
+            document.getElementById("userGuess").value = "";
+        }
+        else if (userInput < randNum) {
+            appendGuess(userInput, "low")
+            document.getElementById("userGuess").value = "";
+        }
+        else {
+            gameWon(userInput)
+    }    
+    }
+    else {
+        alert("Enter a valid number 1-100!")
+    }
+    });
+
+
+function appendGuess(guess, hiLo) {
+    let li = document.createElement("li");
+    li.textContent = `Your guess ${guess} was too ${hiLo}`;
+    document.getElementById("guesses").appendChild(li);
+}
+
+function gameWon(guess) {
+    let li = document.createElement("li");
+    li.textContent = `Your guess ${guess} was correct!  You win!`
+    document.getElementById("guesses").appendChild(li)
+}
+
+function refreshPage() {
+    location.reload();
+}
